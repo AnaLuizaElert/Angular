f84c7dc48c68f4aa3cc161e6eab453510050d424
ANA LUIZA ELERT <ana_elert@estudante.sesisenai.org.br>
2022-10-20 21:57:55 -0300

    Formulário

---
+++ src/app/pages/data-binding/data-binding.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DataBindingComponent } from './data-binding.component';
+import { InterpolacaoComponent } from './interpolacao/interpolacao.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { ClassAndStyleBindingComponent } from './class-and-style-binding/class-and-style-binding.component';
+import { InputsComponent } from '../inputs/inputs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    DataBindingComponent,
+    InterpolacaoComponent,
+    PropertyBindingComponent,
+    ClassAndStyleBindingComponent,
+  ]
+})
+export class DataBindingModule { }
---
+++ src/app/pages/inputs/inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { NOMEM } from "dns";
+
+@Component({
+    selector: 'app-input',
+    templateUrl:"./inputs.componente.html"
+})
+
+
+export class InputsComponent {
+    
+    darConsole(nome: string, genero:string, idade:number){
+        this.nome
+    }
+}
